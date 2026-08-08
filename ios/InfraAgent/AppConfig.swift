93d2ccfa68c5b374c36ae32f1e38cb8b7ddb3bd0
understand-hani <690995838@qq.com>
2026-08-08 22:24:30 +0800

    Update backend tunnel URL

--- ios/InfraAgent/AppConfig.swift
+++ ios/InfraAgent/AppConfig.swift
@@ -4,7 +4,7 @@ struct AppConfig {
     static let appName = "圆周引擎"
     static let backendBaseURLKey = "backend_base_url"
     static let apiTimeoutInterval: TimeInterval = 30
-    static let defaultBackendBaseURL = URL(string: "https://telephony-locator-billing-empirical.trycloudflare.com")!
+    static let defaultBackendBaseURL = URL(string: "https://times-com-submit-vids.trycloudflare.com")!
     private static let legacyDefaultBackendBaseURLs = [
         "https://fairfield-rpm-leo-sacred.trycloudflare.com",
         "https://flex-islands-four-aurora.trycloudflare.com",
@@ -13,7 +13,8 @@ struct AppConfig {
         "https://mounting-harvard-bicycle-grove.trycloudflare.com",
         "https://psychological-merger-cedar-qualifying.trycloudflare.com",
         "https://lemon-somehow-sussex-adrian.trycloudflare.com",
-        "https://telephony-locator-billing-empirical.trycloudflare.com"
+        "https://telephony-locator-billing-empirical.trycloudflare.com",
+        "https://times-com-submit-vids.trycloudflare.com"
     ]
 
     static var backendBaseURL: URL {
