e1f637da5ca5567df537acc9d3655dc0aeb627d8
hani <690995838@qq.com>
2026-08-11 17:00:44 +0800

    Update backend tunnel URL

--- ios/InfraAgent/AppConfig.swift
+++ ios/InfraAgent/AppConfig.swift
@@ -4,7 +4,7 @@ struct AppConfig {
     static let appName = "圆周引擎"
     static let backendBaseURLKey = "backend_base_url"
     static let apiTimeoutInterval: TimeInterval = 30
-    static let defaultBackendBaseURL = URL(string: "https://encouraged-okay-rom-string.trycloudflare.com")!
+    static let defaultBackendBaseURL = URL(string: "https://express-cons-gamma-nose.trycloudflare.com")!
     private static let legacyDefaultBackendBaseURLs = [
         "https://fairfield-rpm-leo-sacred.trycloudflare.com",
         "https://flex-islands-four-aurora.trycloudflare.com",
@@ -19,7 +19,8 @@ struct AppConfig {
         "https://ssl-mirror-unsigned-championships.trycloudflare.com",
         "https://comparable-electron-filename-period.trycloudflare.com",
         "https://explaining-discount-limitations-tribe.trycloudflare.com",
-        "https://needle-mar-diameter-mortality.trycloudflare.com"
+        "https://needle-mar-diameter-mortality.trycloudflare.com",
+        "https://encouraged-okay-rom-string.trycloudflare.com"
     ]
 
     static var backendBaseURL: URL {
